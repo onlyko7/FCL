d9199e335cea36ce24ba0a1fea5c7ee764a5d2c0
chanyoungseo <chanyoungseo@localhost>
2020-07-18 14:29:26 +0900

    파일업로드 기능 추가

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ const utf8 = require("utf8");
 const cron = require("node-cron");
 const app = express();
 
+const multer = require('multer'); // express에 multer모듈 적용 (for 파일업로드)
+const upload = multer({
+				  storage: multer.diskStorage({
+					    destination: function (req, file, cb) {
+					      cb(null, 'public/image/');
+					    },
+					    filename: function (req, file, cb) {
+					      //cb(null, file.originalname);
+					    	cb(null, req.body.filename);
+					    }
+					  }),
+					});
+
 const notice = require('./Dao/Notice.js');
 
 var counter = 1; //접속 카운터
@@ -76,6 +89,15 @@ app.get('/leader_board', function(req, res){
 	
 	console.log(counter++);
 	
+});
+
+app.get('/upload', function(req, res){
+	res.render('upload');
+});
+
+app.post('/upload', upload.single('userfile'), function(req, res){
+  res.send('Uploaded! : '+req.file); // object를 리턴함
+  //console.log(req.file); // 콘솔(터미널)을 통해서 req.file Object 내용 확인 가능.
 });
 
 app.get('/', function(req, res){
